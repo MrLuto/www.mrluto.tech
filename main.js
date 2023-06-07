44dd421dce49d4cc902c39b5f5a77c2aa1ed4e8b
MrLuto <mmudde2@gmail.com>
2023-06-07 22:09:38 +0200

    added a auto get projects feature

---
+++ main.js
@@ -0,0 +1,36 @@
+const username = "MrLuto";
+
+fetch(`https://api.github.com/users/${username}/repos`)
+    .then(response => response.json())
+    .then(data => {
+      // Lus door de array met repositories
+      data.forEach(repo => {
+        const repoName = repo.name;
+        const repoDescription = repo.description;
+        const repoUrl = repo.html_url;
+  
+        // Een GET-verzoek naar de inhoud van de repository maken
+        fetch(`https://api.github.com/repos/${username}/${repoName}/contents`)
+          .then(response => response.json())
+          .then(contents => {
+            // Zoek naar het bestand "thumbnail.webp" in de inhoud van de repository
+            const thumbnailFile = contents.find(file => file.name === "thumbnail.webp");
+  
+            // Als het bestand is gevonden, log de URL
+            var thumbnailUrl = "./assets/images/600x400.webp";
+            if (thumbnailFile) {
+              thumbnailUrl = thumbnailFile.download_url;
+              console.log(`Thumbnail URL voor ${repoName}: ${thumbnailUrl}`);
+            }
+            const projectItem = `<li class="project-item active"><a href="${repoUrl}" target="_blank" aria-label="${repoName}"><figure class="project-img"><div class="project-item-icon-box"><ion-icon name="logo-github"></ion-icon></div><img src="${thumbnailUrl}" alt="Github" width="600" height="400" loading="lazy"></figure><h3 class="project-title">${repoName}</h3><p class="project-category">${repoDescription}</p></a></li>`;
+            document.querySelector(".project-list").innerHTML += projectItem;
+            console.log("---");
+          })
+          .catch(error => {
+            console.error(`Fout bij het ophalen van de inhoud van ${repoName}:`, error);
+          });
+      });
+    })
+    .catch(error => {
+      console.error("Er is een fout opgetreden:", error);
+    });
